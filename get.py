9afa73d9fc058a068e94500966d4f00d157528d6
Chen wz <129849156+Q3dlaXpoaQ@users.noreply.github.com>
2024-04-24 22:11:57 +0800

    Update get.py

--- get.py
+++ get.py
@@ -8,8 +8,7 @@ month = current_date.strftime('%m')
 day = current_date.strftime('%Y%m%d')
 yesterday_date = current_date - timedelta(days=1)
 y_day=day = yesterday_date.strftime('%Y%m%d')
-
-urls =[ f"https://nodefree.org/dy/{year}/{month}/{day}.txt",f"https://tglaoshiji.github.io/nodeshare/{year}/{str(int(month))}/{y_day}.txt"]
+urls =[ f"https://nodefree.org/dy/{year}/{month}/{day}.txt",f"https://tglaoshiji.github.io/nodeshare/{year}/{str(int(month))}/{y_day}.yaml",f"https://clashgithub.github.io/uploads/{year}/{month}/3-{day}.txt"]
 
 def gets():
     for i in range(len(urls)):
@@ -17,13 +16,17 @@ def gets():
             response = requests.get(urls[i],verify=False)
 
             if response.status_code == 200:
-                text_content = response.text
+                text_content = response.text.encode("gbk")   
                 if i==0:
                     with open('nodefree.txt', 'w') as file:
-                        file.write(text_content)
+                        file.write(text_content.decode("gbk"))
                 if i==1:
                     with open('nodeshare.txt', 'w') as file:
-                        file.write(text_content)
+                        file.write(text_content.decode("gbk"))
+                if i==2:
+                    with open('clashgithub.txt', 'w') as file:
+                        print(text_content)
+                        file.write(text_content.decode("gbk"))
                 print("Text content saved to .txt")
             else:
                 print("Failed to fetch text content from URL:", urls[i])
