a9088571e57566c505a320d606fe623c788f9352
Chen wz <129849156+Q3dlaXpoaQ@users.noreply.github.com>
2024-07-02 20:20:27 +0800

    Add files via upload

--- get.py
+++ get.py
@@ -7,8 +7,12 @@ year = current_date.strftime('%Y')
 month = current_date.strftime('%m')
 day = current_date.strftime('%Y%m%d')
 yesterday_date = current_date - timedelta(days=1)
-y_day=day = yesterday_date.strftime('%Y%m%d')
-urls =[ f"https://nodefree.org/dy/{year}/{month}/{day}.txt",f"https://tglaoshiji.github.io/nodeshare/{year}/{str(int(month))}/{y_day}.yaml",f"https://clashgithub.github.io/uploads/{year}/{month}/3-{day}.txt"]
+y_day = yesterday_date.strftime('%Y%m%d')
+
+urls =[ f"https://nodefree.org/dy/{year}/{month}/{day}.txt",f"https://tglaoshiji.github.io/nodeshare/{year}/{str(int(month))}/{day}.yaml"]
+
+for i in range(5):
+    urls.append(str(f"https://clashgithub.github.io/uploads/{year}/{month}/{i}-{day}.txt"))
 
 def gets():
     for i in range(len(urls)):
@@ -23,10 +27,9 @@ def gets():
                 if i==1:
                     with open('nodeshare.txt', 'w') as file:
                         file.write(text_content.decode("gbk"))
-                if i==2:
-                    with open('clashgithub.txt', 'w') as file:
-                        print(text_content)
-                        file.write(text_content.decode("gbk"))
+                if i>=2:
+                    with open(f'clashgithub{i-2}.txt', 'w') as file:                        file.write(text_content.decode("gbk"))
+                
                 print("Text content saved to .txt")
             else:
                 print("Failed to fetch text content from URL:", urls[i])
